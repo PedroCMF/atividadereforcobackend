bead6586bd9cbba8762e25062c21267317b65014
PedroCMF <pedrounib@gmail.com>
2023-07-18 23:19:00 -0300

    aula reforso

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto.Classes
+{
+    public abstract class Pessoa 
+    {
+        public string? Nome {get;set;}
+        public float Rendimento { get; set; }
+
+        public  void NomeMetodo()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Projeto.Classes;
+
+string opcao = "";
+PessoaJuridica MetodosPJ = new PessoaJuridica();
+do
+{
+    Console.WriteLine(@"
+
+    1 - Cadastrar PJ
+    2 - Sair
+
+    ");
+
+    opcao = Console.ReadLine()!;
+
+    if (opcao == "1")
+    {
+        PessoaJuridica pj = new PessoaJuridica();
+
+        Console.WriteLine("Digite o nome da Pessoa Juridica: ");
+        pj.Nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o rendimento da Pessoa Juridica");
+        pj.Rendimento = float.Parse(Console.ReadLine()) ; //10000
+
+        Console.WriteLine("Digite o CNPJ da PJ");
+        pj.CNPJ = Console.ReadLine();
+
+        MetodosPJ.Inserir(pj);
+    }
+} while (opcao != "2");
+
